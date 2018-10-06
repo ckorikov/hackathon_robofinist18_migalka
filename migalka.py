c150c9fab0c811d83b80b57f523ec53cc800e7fd
ckorikov <constantine.korikov@gmail.com>
2018-10-06 15:18:48 +0300

    Test img

--- migalka.py
+++ migalka.py
@@ -2,16 +2,33 @@
 
 import rospy
 from geometry_msgs.msg import Vector3, Twist
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+
 # from sensor_msgs.msg import LaserScan
 
 STOP = Twist(linear=Vector3(0,0,0), angular=Vector3(0,0,0))
+bridge = CvBridge()
 
 # def process_scan(msg):
 #     print(msg)
 
+def image_callback(msg):
+    print("Received an image!")
+    try:
+        # Convert your ROS Image message to OpenCV2
+        cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
+    except CvBridgeError, e:
+        print(e)
+    else:
+        # Save your OpenCV2 image as a jpeg
+        cv2.imwrite('camera_image.jpeg', cv2_img)
+
 print("Start!")
 rospy.init_node('migalka_core') # Initialise a ROS node
-publisher = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size=1)
+# publisher = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size=1)
+rospy.Subscriber("/camera/rgb/image_raw", Image, image_callback)
 # subscriber = rospy.Subscriber('/scan', LaserScan, process_scan)
 
 rate = rospy.Rate(2)
